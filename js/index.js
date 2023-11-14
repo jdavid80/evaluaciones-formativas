1ddb4beca928d89484f4116909c8429e1e48cc12
jdavid80 <jdcalderong@gmail.com>
2023-11-14 06:11:04 -0500

    add users

--- js/index.js
+++ js/index.js
@@ -1,9 +1,18 @@
 const btnValidarUsuario = document.getElementById("btnValidarUsuario");
 let login = document.getElementById("login");
 let password = document.getElementById("password");
+let idUsuario = document.getElementById("idUsuario");
+let rolUsuario = document.getElementById("rolUsuario");
 
+window.addEventListener("load", () => {
+    login.focus();
+});
 btnValidarUsuario.addEventListener("click", () => {
-    if ((login.value == "12265812")  && (password.value == "12265812")) 
+    if ((login.value == "1076908953")  && (password.value == "1076908953")) 
+    {
+        location.href = "main-page-jose-dario.html";
+    }
+    else if ((login.value == "12265812")  && (password.value == "12265812")) 
     {
         location.href = "main-page.html";
     }
@@ -13,12 +22,16 @@ btnValidarUsuario.addEventListener("click", () => {
 });
 document.addEventListener("keyup", function(event) {
     if(event.key == 'Enter') {
-        if ((login.value == "admin")  && (password.value == "tesis")) 
-    {
-        location.href = "main-page.html";
-    }
-    else {
-        alert("Usuario y/o contraseña incorrectos. Intentelo de nuevo.");
-    }
+        if ((login.value == "1076908953")  && (password.value == "1076908953")) 
+        {
+            location.href = "main-page-jose-dario.html";
+        }
+        else if ((login.value == "12265812")  && (password.value == "12265812")) 
+        {
+            location.href = "main-page.html";
+        }
+        else {
+            alert("Usuario y/o contraseña incorrectos. Intentelo de nuevo.");
+        }
     }
 });
